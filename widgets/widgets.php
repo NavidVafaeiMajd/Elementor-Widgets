1d45ca7b9e73932601037ce7653f2e5148b20419
navid <vafaermn@gmail.com>
2025-04-21 00:27:55 +0330

    base of html

--- widgets/widgets.php
+++ widgets/widgets.php
@@ -52,20 +52,76 @@ class My_Custom_Widget extends \Elementor\Widget_Base {
     protected function render() {
         $settings = $this->get_settings_for_display();
         ?>
-        <div class="my-widget">
-            <h2>NAVID<?php echo $settings['title']; ?></h2>
-            <button><?php echo $settings['button_text']; ?></button>
-        </div>
+	<div class="mywidget_card_container">
+		<div class="mywidget_card mywidget-card-front">
+			<div class="mywidget_img_wrapper">
+				<img src="/asset/img/0.jpg" alt="Card image" title="Photo by Joseph Barrientos for Unsplash">
+				<div class="mywidget_btn">
+					<div class="mywidget_round_wrapper"><a href="#" class="mywidget-read-more">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" with="30" height="30" fill="white">
+								<path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
+							</svg></a>
+					</div>
+				</div>
+			</div>
+			<div class="mywidget_container_text">
+				<h2>Where in the World Will You Go Next ?</h2>
+				<p>From coastal escapes to mountain retreats, your journey begins here.
+					Each destination is handpicked for wanderers with a taste for wonder.
+					Pack your bags—your next chapter is just a card away. </p>
+			</div>
+		</div>
+
+		<div class="mywidget_card mywidget-card-back">
+			<h3>Where Every Sunset Feels Like Magic</h3>
+			<h1>Maui, Hawai‘i</h1>
+
+			<p>Savor island flavors, from fresh poke bowls to sweet shaved ice by the shore.
+				End your days with golden sunsets over the Pacific and the sound of waves lapping at your feet.
+				In Maui, every bite and every view is a memory in the making.</p>
+			<div class="mywidget_btn_back">
+				<a href="#" class="mywidget-read-more">Read more</a>
+			</div>
+		</div>
+	</div>
         <?php
     }
 
     // پیش‌نمایش زنده توی ویرایشگر المنتور
     protected function content_template() {
         ?>
-        <div class="my-widget">
-            <h2>{{{ settings.title }}}</h2>
-            <button>{{{ settings.button_text }}}</button>
-        </div>
+	<div class="mywidget_card_container">
+		<div class="mywidget_card mywidget-card-front">
+			<div class="mywidget_img_wrapper">
+				<img src="/asset/img/0.jpg" alt="Card image" title="Photo by Joseph Barrientos for Unsplash">
+				<div class="mywidget_btn">
+					<div class="mywidget_round_wrapper"><a href="#" class="mywidget-read-more">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" with="30" height="30" fill="white">
+								<path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z" />
+							</svg></a>
+					</div>
+				</div>
+			</div>
+			<div class="mywidget_container_text">
+				<h2>Where in the World Will You Go Next ?</h2>
+				<p>From coastal escapes to mountain retreats, your journey begins here.
+					Each destination is handpicked for wanderers with a taste for wonder.
+					Pack your bags—your next chapter is just a card away. </p>
+			</div>
+		</div>
+
+		<div class="mywidget_card mywidget-card-back">
+			<h3>Where Every Sunset Feels Like Magic</h3>
+			<h1>Maui, Hawai‘i</h1>
+
+			<p>Savor island flavors, from fresh poke bowls to sweet shaved ice by the shore.
+				End your days with golden sunsets over the Pacific and the sound of waves lapping at your feet.
+				In Maui, every bite and every view is a memory in the making.</p>
+			<div class="mywidget_btn_back">
+				<a href="#" class="mywidget-read-more">Read more</a>
+			</div>
+		</div>
+	</div>
         <?php
     }
 }
